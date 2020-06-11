b946c422177b346a3ae8df771a1e3e40ec70cb0f
lxvlg <8783637+lxvlg@users.noreply.github.com>
2020-06-11 16:50:36 +0300

    fixed bash script

--- test.sh
+++ test.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 set -e
+set -u
 
 OLD_FILE=/tmp/old
 
 NEW_FILE=/tmp/new
 
-if [ ! -d $OLD_FILE ] ; then
+if [ -f $OLD_FILE ] ; then
+	echo OK: file $OLD_FILE exists
+else
 	echo ERROR: file $OLD_FILE doesn't' exist
 	exit 1
 fi
 
-[ -d $NEW_FILE ] && rm -f $NEW_FILE
+if [ -f $NEW_FILE ] && rm -f $NEW_FILE; then
+	echo OK: $NEW_FILE is file and deleted
+fi
 
 content=`cat /tmp/old`
 
-if [ $content == "" ] ;
-	echo Old file is empty
-	exit
+if [ "$content" == "" ] ; then
+	echo ERROR: $OLD_FILE is empty
+	exit 1
+else
+	echo OK: $OLD_FILE have content
 fi
 
-count=0
+count=1
+
 for line in $content ; do
-	new_file_content=$(echo -e "$new_file_content\n$count: $line")
+	new_file_content=$(echo -e "$count: $line")
 	count=$(($count + 1))
+	echo $new_file_content >> $NEW_FILE
 done
 
-echo $new_file_content >  $NEW_FILE
-
-sed 's/REAL_PASSWD/****/g' $NEW_FILE > $NEW_FILE
+sed -i 's/REAL_PASSWD/****/g' $NEW_FILE
+#с точки зрения безопасности REAL_PASSWD надо-бы читать из фаила или из пользовательского ввода при запуске.
